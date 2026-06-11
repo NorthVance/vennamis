de5905ecff651ef9e09c73200256b8e3a065571d
2712 <87257092+NorthVance@users.noreply.github.com>
2026-06-11 14:30:06 +0700

    Update1.5

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Providing-work/', // <--- ต้องมีบรรทัดนี้ ไม่งั้น CSS/รูป จะหาย
+  base: '/Providing-work/',  
 })
